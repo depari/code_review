86d7fe548b0fc233cc8882c49a8028f0e18a8eff
depari <cryingfor@gmail.com>
2019-08-13 16:30:15 +0900

    divide UnitTest
    Codereveiw 반영
     - unittest 분리
     - indent 수정

--- shopping_test.cc
+++ shopping_test.cc
@@ -5,21 +5,7 @@
 //   ... test body ...
 // }
 
-
-TEST(shopping_list_test, goods_object_test) {
-  Goods testobject;
-
-  // Test 1 - Initalize
-  EXPECT_EQ(testobject.GetIndex(), 0);
-  ASSERT_EQ(testobject.GetPrice(), 0);
-
-  // Test 2 - Set Member Variable
-  testobject.SetGoodsInfo(1, 1000);
-  EXPECT_EQ(testobject.GetIndex(), 1);
-  ASSERT_EQ(testobject.GetPrice(), 1000);
-}
-
-TEST(shopping_list_test, shopping_object_test) {
+Shopping GenerateShoppingListForTest() {
   Shopping shopping_object;
   Goods goods_test_object1;
   Goods goods_test_object2;
@@ -40,25 +26,60 @@ TEST(shopping_list_test, shopping_object_test) {
   shopping_object.PushList(goods_test_object4);
   shopping_object.PushList(goods_test_object5);
 
+  return shopping_object;
+
+}
+
+
+TEST(shopping_list_test, goods_object_test) {
+  Goods testobject;
+
+  // Test 1 - Initalize
+  EXPECT_EQ(testobject.GetSerialNumber(), 0);
+  ASSERT_EQ(testobject.GetPrice(), 0);
+
+  // Test 2 - Set Member Variable
+  testobject.SetGoodsInfo(1, 1000);
+  EXPECT_EQ(testobject.GetSerialNumber(), 1);
+  ASSERT_EQ(testobject.GetPrice(), 1000);
+}
+
+
+
+
+TEST(shopping_list_test, shopping_object_last_element_test) {
+  Shopping shopping_object = GenerateShoppingListForTest();
   // Test 0 - Last element test
   EXPECT_EQ((shopping_object.GetLastElement()).GetPrice(), 30000);
-  ASSERT_EQ((shopping_object.GetLastElement()).GetIndex(), 2);
+  ASSERT_EQ((shopping_object.GetLastElement()).GetSerialNumber(), 2);
 
-  // Test 1 - Sort by Price
-  shopping_object.SortByPrice();
 
-  // Price of last element is 49900
-  EXPECT_EQ((shopping_object.GetLastElement()).GetPrice(), 49900);
-  ASSERT_EQ((shopping_object.GetLastElement()).GetIndex(), 4);
+}
+
+TEST(shopping_list_test, shopping_object_sortbyindex_test) {
+  Shopping shopping_object = GenerateShoppingListForTest();
 
   // Test 2 - Sort by Index
   shopping_object.SortByIndex();
 
   // Index of last element is 5
   EXPECT_EQ((shopping_object.GetLastElement()).GetPrice(), 4500);
-  ASSERT_EQ((shopping_object.GetLastElement()).GetIndex(), 5);
+  ASSERT_EQ((shopping_object.GetLastElement()).GetSerialNumber(), 5);
 }
+
+
+TEST(shopping_list_test, shopping_object_sortbyprice_test) {
+  Shopping shopping_object = GenerateShoppingListForTest();
+
+  // Test 1 - Sort by Price
+  shopping_object.SortByPrice();
+
+  // Price of last element is 49900
+  EXPECT_EQ((shopping_object.GetLastElement()).GetPrice(), 49900);
+  ASSERT_EQ((shopping_object.GetLastElement()).GetSerialNumber(), 4);
+}
+
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
